a2316eb30d026973051e0db5ff8cfae6cb1723fb
Nikita Gusakov <dev@nkt.me>
2015-10-22 12:51:18 +0300

    Added all command

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
 #!/usr/bin/env node
-
+'use strict';
 var fs = require('fs');
 var path = require('path');
+var execFileSync = require('child_process').execFileSync;
 var program = require('commander');
 var pkg = require('../package.json');
 
-program
-  .version(pkg.version);
+program.version(pkg.version);
 
 var benchmarksPath = path.join(__dirname, 'benchmarks');
-fs.readdirSync(benchmarksPath).forEach(function(filename) {
+function runBenchmark(filename) {
+  var filepath = path.join(benchmarksPath, filename);
+  execFileSync(process.argv[0], [
+    '--harmony',
+    filepath
+  ], {
+    stdio: ['ignore', process.stdout, process.stderr],
+    env: {
+      NODE_ENV: 'production'
+    }
+  });
+}
+
+var benchmarks = fs.readdirSync(benchmarksPath);
+program
+  .command('all')
+  .action(function() {
+    benchmarks.forEach(runBenchmark);
+  });
+
+benchmarks.forEach(function(filename) {
   var command = program.command(filename.replace(/\.js$/, ''));
   command.action(function() {
-    var execFileSync = require('child_process').execFileSync;
-
-    var filepath = path.join(benchmarksPath, filename);
-    execFileSync(process.argv[0], [
-      '--harmony',
-      filepath
-    ], {
-      stdio: ['ignore', process.stdout, process.stderr],
-      env: {
-        NODE_ENV: 'production'
-      }
-    });
+    runBenchmark(filename);
   });
 });
 
